df91843be4ae9ccd4ff11de7e035c800ce356cb4
brian070069 <kkipngeno409@gmail.com>
2025-08-11 14:32:24 +0300

    any modifications for lists backed by arrays usually affect the array, the changes also impact on the backing array

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,25 +1,29 @@
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class Main{
-    public static void main(String[]args) {
-        String[] originalArray = new String[] {"First","Second","Third"};
-        var originalList = Arrays.asList(originalArray);
+    public static void main(String[] args) {
+        String[] names = new String[] {"Kipngeno","Atieno","Adhiambo","Kipkirui","Kibet"};
+        var namesAsList = Arrays.asList(names);
 
-        originalList.set(0,"one");
-        System.out.println("list:"+originalList);
-        System.out.println("Array :"+Arrays.toString(originalArray));
+        namesAsList.set(0,"kevooo");
 
-        originalList.sort(Comparator.naturalOrder());
-        System.out.println("Array after sorting :"+Arrays.toString(originalArray));
+        System.out.println(namesAsList);
+        System.out.println(Arrays.toString(names));
 
-//        originalList.remove("one");
-        List<String> newList = Arrays.asList("Sunday","Monday","Tuesday");
-        System.out.println(newList);
+        namesAsList.sort(Comparator.naturalOrder());
+        System.out.println(Arrays.toString(names));
 
 
+        ArrayList<String> stringList = new ArrayList<> (List.of("Jan","Feb","Mar"));
+        String[] stringArray = stringList.toArray( new String[0]);
+
+        System.out.println(Arrays.toString(stringArray));
     }
+
+
 }
